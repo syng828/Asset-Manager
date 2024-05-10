effd1df3ddf308aafd5af51b18334086edfbf94d
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-05-09 17:23:33 -0700

    Browse Location finished

--- src/application/controller/BrowseLocationController.java
+++ src/application/controller/BrowseLocationController.java
@@ -2,10 +2,13 @@ package application.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Date;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -14,6 +17,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.TextArea;
 import mapping.Asset;
 import mapping.AssetHandler;
@@ -21,13 +26,15 @@ import mapping.TagHandler;
 
 public class BrowseLocationController {
 	
+	@FXML AnchorPane pane;
 	@FXML ChoiceBox<String> lDrop;
 	private HashMap<String, Integer> lMap;
 	@FXML TableView<Asset> locTable;
 	@FXML TableColumn<Asset, String> aCol;
-	@FXML TableColumn <Asset, String> cCol;
-	@FXML TableColumn <Asset, String> lCol;
+	//@FXML TableColumn <Asset, String> cCol;
+	//@FXML TableColumn <Asset, String> lCol;
 	@FXML Button searchBtn;
+	@FXML Button examineBtn;
 	
 	private void initLocationDropDown()  { 
 		lMap = TagHandler.getLocationMap();
@@ -39,6 +46,36 @@ public class BrowseLocationController {
 		TagHandler.initMap();
 		initLocationDropDown();
 	}
+	
+	@FXML public void examineAsset() { 
+		if (locTable.getSelectionModel().getSelectedItem() == null) { 
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+		    alert.setTitle("No Asset Selected");
+		    alert.setContentText("Please select an asset."); 
+		    alert.showAndWait();
+		} else { 
+			Asset asset = locTable.getSelectionModel().getSelectedItem();
+			AssetHandler.selectAsset(asset);  //selects current asset to be used when editing
+			loadFXML("view/ExamineAsset.fxml");
+		}
+	}
+	
+	private void loadFXML(String fxmlPath) { 
+		HBox mainBox = (HBox) pane.getParent();
+		URL url = getClass().getClassLoader().getResource(fxmlPath); 
+		AnchorPane panel;
+		try {
+			panel = (AnchorPane) FXMLLoader.load(url);
+			if (mainBox.getChildren().size() > 1 ) //Removes right AnchorPane beforehand to avoid overlapping views
+				mainBox.getChildren().remove(1);
+			mainBox.getChildren().add(panel);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}	
+	
 	
 	public void displayLocTable() {
 		if (lDrop.getSelectionModel().getSelectedItem() == null) { 
@@ -59,23 +96,25 @@ public class BrowseLocationController {
 			}
 			//sets values with the specified attributes
 			aCol.setCellValueFactory(new PropertyValueFactory<Asset, String>("name")); //gets property from assets
+			/*
 			cCol.setCellValueFactory(cellData -> {
 			    String categoryName = cellData.getValue().getCategoryName(); //creates a special string property from category name for each asset
 			    return new SimpleStringProperty(categoryName);});  
 			lCol.setCellValueFactory(cellData -> {
 			    String locationName = cellData.getValue().getLocationName();
 			    return new SimpleStringProperty(locationName);}); 
-		
+			*/
 			locTable.getColumns().clear();
 			locTable.getItems().clear();
 			locTable.getColumns().add(aCol);
-			locTable.getColumns().add(cCol);
-			locTable.getColumns().add(lCol);
+			//locTable.getColumns().add(cCol);
+			//locTable.getColumns().add(lCol);
 			     
 			for(Asset ast: a ) {
 				locTable.getItems().add(ast);
 			}
 		}
 	}
+	
 	
 }
